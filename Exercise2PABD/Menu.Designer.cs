7e253982a4405e2a28fa03a27463473db46af685
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-06-15 10:38:16 +0700

    menghapus nama button

--- Exercise2PABD/Menu.Designer.cs
+++ Exercise2PABD/Menu.Designer.cs
@@ -29,31 +29,10 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Image = global::Exercise2PABD.Properties.Resources.data_spreadsheet;
-            this.button1.Location = new System.Drawing.Point(49, 166);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(296, 223);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Image = global::Exercise2PABD.Properties.Resources.up_and_down;
-            this.button2.Location = new System.Drawing.Point(440, 166);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(313, 223);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -65,6 +44,25 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Menu Utama";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::Exercise2PABD.Properties.Resources.up_and_down;
+            this.button2.Location = new System.Drawing.Point(440, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(313, 223);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Exercise2PABD.Properties.Resources.data_spreadsheet;
+            this.button1.Location = new System.Drawing.Point(49, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(296, 223);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Menu
             // 
