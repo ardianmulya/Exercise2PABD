31851dff68d6a99a289499191d88d2d777a5a2ea
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2023-06-15 10:19:24 +0700

    Add project files.

---
+++ Exercise2PABD/Menu.cs
@@ -0,0 +1,31 @@
+﻿using Data_MHS1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise2PABD
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Data_MHS1 Data = new Data_MHS1
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
